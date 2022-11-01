eb8374fb8f09f981e363cd509fa7fb6d708466be
Dusuna <dusunax@gmail.com>
2022-11-02 02:14:36 +0900

    Feature Header GNB, Mobile Hamberger button

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const Header: React.FC<{}> = (props) => {
-  return <div>Header</div>;
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import { StLogo, StMenu, StNav, StHamburger } from "./style";
+
+const Header: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const profileUrl: string = "";
+  const router = useRouter();
+  const menuRef = useRef<HTMLUListElement>();
+  const path = router.pathname;
+
+  const menuClickHandler = (e: { target: HTMLInputElement }) => {
+    const menuList = Array.from(menuRef!.current!.children);
+    const target = e.target.closest("li");
+
+    for (const li in menuList) {
+      menuList[li]!.classList.remove("active");
+    }
+
+    if (target) {
+      const targetPath = target.dataset.path;
+      router.push(targetPath);
+      target.classList.add("active");
+    }
+  };
+
+  const activeCurrentList = (path: string) => {
+    const menuList = Array.from(menuRef!.current!.children);
+    menuList.map((li) => {
+      if (li.dataset.path === path) li.classList.add("active");
+    });
+  };
+
+  const logoClickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    router.push("/");
+    menuClickHandler(e);
+  };
+
+  useEffect(() => {
+    activeCurrentList(path);
+  }, []);
+
+  console.log(showMenu);
+
+  return (
+    <StNav>
+      <StHamburger on={!showMenu}>
+        <label
+          htmlFor="hamburger"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+        >
+          <img src="/icons/icon_nav_menu.svg" alt="햄버거 메뉴" />
+        </label>
+      </StHamburger>
+
+      <Link
+        href="/"
+        onClick={(e: React.MouseEvent<HTMLElement>) => logoClickHandler(e)}
+      >
+        <StLogo profileUrl={profileUrl}>Logo</StLogo>
+      </Link>
+
+      <div className="right">
+        <StMenu
+          on={showMenu}
+          onClick={(e: React.MouseEvent<HTMLUListElement>) =>
+            menuClickHandler(e)
+          }
+          ref={menuRef}
+        >
+          <li data-path="/salons">정기 모임</li>
+          <li data-path="/events">이벤트</li>
+          <li data-path="/community">소모임</li>
+          <li data-path="/about">About</li>
+        </StMenu>
+
+        <img src="icons/img_profile_default.svg" alt="프로필 사진" />
+      </div>
+    </StNav>
+  );
 };
 
 export default Header;
---
+++ src/components/organisms/header/style.ts
@@ -0,0 +1,91 @@
+import styled from "@emotion/styled";
+import navMenu from "../../../../public/icons/icon_nav_menu.svg";
+
+export const StNav = styled.nav`
+  height: 4rem;
+
+  padding: 0 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: relative;
+
+  .right {
+    display: flex;
+    gap: 1rem;
+  }
+`;
+
+export const StLogo = styled.div<{ profileUrl: string }>`
+  width: 65px;
+
+  background: url(${(props) => props.profileUrl}) center / cover;
+
+  font-size: 2rem;
+  text-align: center;
+`;
+
+export const StMenu = styled.ul<{ on: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  li {
+    padding: 0 1rem;
+
+    color: #828282;
+
+    transition: all 0.2s;
+    cursor: pointer;
+  }
+
+  li.active,
+  li:hover {
+    color: #111;
+  }
+
+  @media (max-width: 900px) {
+    width: 100%;
+
+    position: absolute;
+    top: 100%;
+    left: 0;
+
+    flex-flow: column;
+
+    background-color: #fff;
+    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+
+    overflow: hidden;
+
+    transition: height 0.4s 0.2s;
+
+    li {
+      padding: 1.2rem 2rem;
+      height: 0px;
+      width: 100%;
+      height: auto;
+    }
+
+    ${(props) => (props.on ? "height: 400%" : "height: 0;")}
+  }
+`;
+
+export const StHamburger = styled.div<{ on: boolean }>`
+  input {
+    display: none;
+  }
+  label {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    ${(props) => (props.on ? "opacity: 0.4" : "")}
+  }
+
+  display: none;
+  @media (max-width: 900px) {
+    display: block;
+  }
+`;
